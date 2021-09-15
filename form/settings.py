722bddf73e6a49e75669b0265464745ed8696a10
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2021-09-16 00:50:44 +0530

    Update settings.py

--- form/settings.py
+++ form/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "d2socials.herokuapp.com", "127.0.0.1", "harini18.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "d2socials.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
