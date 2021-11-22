7e82e8139d0f0fec830b9867b36ced86c3dd7e01
ironnicko <nickonicko779@gmail.com>
2021-11-22 12:57:01 +0530

    turned off debug mode

--- form/settings.py
+++ form/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "django-insecure-g67%y6f%lp6dgftb7q=xaeyiy206gnxi5r^ql^b2%(e$-b3ntt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "d2socials.herokuapp.com", "127.0.0.1"]
 
--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
