863bfaec9459da9cab177d15bf5a2b588d578772
ironnicko <nickonicko779@gmail.com>
2021-11-27 03:40:25 +0530

    added the new storage part

--- form/settings.py
+++ form/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # MEDIA_URL = "/images/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
