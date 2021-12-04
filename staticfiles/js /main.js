df7df8de8be587002e2d9adb9b7be572a46cda4a
ironnicko <nickonicko779@gmail.com>
2021-12-05 04:00:00 +0530

    site fixed

--- staticfiles/js /main.js
+++
@@ -1,7 +0,0 @@
-var item = $('.my-name');
-var bac = $('body');
-var change = {"background-image": "url('/srm2.jpg')"};
-body.css(change)
-item.on('click', function(){
-    $(this).toggleClass('turnBlue');
-})
