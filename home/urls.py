cbd73dbe6d11a1c52814c00c39b0a7753d8d9566
ironnicko <nickonicko779@gmail.com>
2021-12-29 23:26:54 +0530

    added regex to url routing

--- home/urls.py
+++ home/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('create/', views.create, name="create_view"),
-    path("", views.view, name="home_view"),
-    path('api/', views.apiOverview, name="api_overview"),
-    path("api/people-names/", views.peopleNames, name="people-names")
+    re_path(r'^create/?$', views.create, name="create_view"),
+    re_path(r"^/?$", views.view, name="home_view"),
+    re_path(r'^api/?$', views.apiOverview, name="api_overview"),
+    re_path(r"^api/people-names/?$", views.peopleNames, name="people-names")
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
